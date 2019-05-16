9f80a1019b717bd4fbfba948a98f8a153ac02e8f
Erika Loots <43708340+Ignisstorm@users.noreply.github.com>
2019-05-16 10:12:12 +0200

    Add files via upload

---
+++ chat_model.dart
@@ -0,0 +1,57 @@
+
+
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+new ChatModel(
+  name: "Erika Loots",
+  message: "Hi, I didn't have time to go shop",
+  time: '15:30',
+  avatarUrl: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+),
+
+ChatModel(
+  name: "Sarah Birellis",
+  message: "No worries, I can go later",
+  time: '14:30',
+  avatarUrl: "https://images.unsplash.com/photo-1450297350677-623de575f31c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+),
+
+ChatModel(
+  name: "Jane Victor",
+  message: "Party tomorrow night girls?",
+  time: '18:30',
+  avatarUrl: "https://images.unsplash.com/photo-1498387727476-b30055b2ef21?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+),
+
+ChatModel(
+  name: "Tucker Kyle",
+  message: "Remember to add me to your email list",
+  time: '12:30',
+  avatarUrl: "https://images.unsplash.com/photo-1489980478712-2ab535aa775f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+),
+
+ChatModel(
+  name: "Jess Simpson",
+  message: "Fashion show tomorrow 19:00!!! ",
+  time: '09:30',
+  avatarUrl: "https://images.unsplash.com/photo-1498842812179-c81beecf902c?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+),
+
+ChatModel(
+  name: "Ryan Blake",
+  message: "Pop in to my shop later, I,ve got a surprise",
+  time: '18:30',
+  avatarUrl: "https://images.unsplash.com/photo-1499083773823-5000fa2b23e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=646&q=80",
+)
+
+];
+
+
